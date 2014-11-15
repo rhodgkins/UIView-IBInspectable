eb6f647fcc14b291bb2c6a489986af75280bea86
Rich Hodgkins <rhodgkins@gmail.com>
2014-11-15 18:09:56 +0000

    Swift files and tests

---
+++ Tests/UIView+IBInspectable/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UIView+IBInspectable
+//
+//  Created by Richard Hodgkins on 15/11/2014.
+//  Copyright (c) 2014 Rich H. All rights reserved.
+//
+
+import UIKit
+
+public class ViewController: UIViewController {
+    
+    @IBOutlet weak public var noneView: UIView!
+    @IBOutlet weak public var cornerRadiusView: UIView!
+    @IBOutlet weak public var borderWidthView: UIView!
+    @IBOutlet weak public var borderColorView: UIView!
+    @IBOutlet weak public var shadowColorView: UIView!
+    @IBOutlet weak public var shadowOpacityView: UIView!
+    @IBOutlet weak public var shadowOffsetView: UIView!
+    @IBOutlet weak public var shadowRadiusView: UIView!
+    @IBOutlet weak public var allView: UIView!
+}
+
